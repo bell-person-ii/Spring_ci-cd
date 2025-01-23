5a1e436b70135f31024b82b5bb34d72e08f8b698
bell-person-ii <leeji88640@gmail.com>
2025-01-23 15:31:54 +0900

    init commit

---
+++ src/main/java/me/dev/cicd_test/controller/HelloController.java
@@ -0,0 +1,15 @@
+package me.dev.cicd_test.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> test(){
+        return new ResponseEntity<>("Hello Every Body!!",HttpStatus.OK);
+    }
+}
---
+++ src/test/java/me/dev/cicd_test/CicdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dev.cicd_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
